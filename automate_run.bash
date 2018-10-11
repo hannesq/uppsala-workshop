92ad5537d6952ab6661bc80ad40a3dfcd000bc9a
Hannes Keck <htux@tutamail.com>
2018-10-11 15:10:57 +0200

    added automate_run.bash script in bash

---
+++ automate_run.bash
@@ -0,0 +1,20 @@
+# define variables
+ # creating results var as the current wd
+RESULTS_DIR=/home/hk/PhD/02_Courses/software_carpentry/bash/data-shell/results
+ # input directory
+INPUT_DIR=/home/hk/PhD/02_Courses/software_carpentry/bash/data-shell/molecules
+
+
+# dont change from here execpt you know what you are doing!
+
+rm -rf $RESULTS_DIR
+mkdir $RESULTS_DIR
+
+touch $RESULTS_DIR/length.txt
+
+for filename in $INPUT_DIR/*.pdb
+  do 
+    wc -l $filename | sort -n | head -n 1 >> $RESULTS_DIR/length.txt  # use echo for 'dry runs'
+done
+
+cat $RESULTS_DIR/length.txt
